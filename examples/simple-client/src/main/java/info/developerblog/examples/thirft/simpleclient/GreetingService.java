68c722ea52cc1265143731686e285689890c3556
aatarasoff <aatarasoff@gmail.com>
2016-02-15 17:23:27 +0300

    add timeouts to TTransport Thrift clients

--- examples/simple-client/src/main/java/info/developerblog/examples/thirft/simpleclient/GreetingService.java
+++ examples/simple-client/src/main/java/info/developerblog/examples/thirft/simpleclient/GreetingService.java
@@ -15,7 +15,14 @@ public class GreetingService {
     @ThriftClient(serviceId = "greeting-service", path = "/api")
     TGreetingService.Client client;
 
+    @ThriftClient(serviceId = "greeting-service-with-timeouts", path = "/api")
+    TGreetingService.Client clientWithTimeout;
+
     public String getGreeting(String lastName, String firstName) throws TException {
         return client.greet(new TName(firstName, lastName));
+    }
+
+    public String getGreetingWithTimeout(String lastName, String firstName) throws TException {
+        return clientWithTimeout.greet(new TName(firstName, lastName));
     }
 }
