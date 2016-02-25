62db1ca35b712df479f0cf6d8dbd1f4aa69e9e73
Горемыкин Алексей Иванович <AIGoremykin@alfabank.ru>
2016-02-25 18:14:57 +0400

    removed dynamic jdk proxy tests since they are not required

--- examples/simple-client/src/main/java/info/developerblog/examples/thirft/proxyclient/jdkproxy/JdkProxyGreetingService.java
+++
@@ -1,8 +0,0 @@
-package info.developerblog.examples.thirft.proxyclient.jdkproxy;
-
-import org.apache.thrift.TException;
-
-public interface JdkProxyGreetingService {
-
-    String getGreeting(String lastName, String firstName) throws TException;
-}
--- examples/simple-client/src/test/java/info/developerblog/examples/thirft/proxyclient/jdkproxy/JdkProxyTestConfiguration.java
+++
@@ -1,8 +0,0 @@
-package info.developerblog.examples.thirft.proxyclient.jdkproxy;
-
-import org.springframework.context.annotation.ComponentScan;
-
-@ComponentScan
-public class JdkProxyTestConfiguration {
-
-}
