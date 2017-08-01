19643ab4bafe800d81163a80eb3d920c6b1b26f8
Tolkachev Kirill <lavcraft@gmail.com>
2017-08-01 17:54:57 +0300

    Customize Application for testing

--- examples/simple-client/src/test/java/info/developerblog/examples/thirft/proxyclient/async/TAsyncGreetingServiceHandlerTests.java
+++ examples/simple-client/src/test/java/info/developerblog/examples/thirft/proxyclient/async/TAsyncGreetingServiceHandlerTests.java
@@ -6,12 +6,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(webEnvironment = RANDOM_PORT)
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SimpleClientApplication.class, webEnvironment = RANDOM_PORT)
 public class TAsyncGreetingServiceHandlerTests {
 
     @Autowired
