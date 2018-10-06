82ba345f3c18aba2957b065823044b779303b13e
Tobias Bales <tobias.bales@gmail.com>
2018-10-06 10:58:01 +0200

    Add request duration metric

--- autoconfigure/src/main/java/ru/trylogic/spring/boot/thrift/aop/MetricsThriftMethodInterceptor.java
+++ autoconfigure/src/main/java/ru/trylogic/spring/boot/thrift/aop/MetricsThriftMethodInterceptor.java
@@ -1,22 +1,50 @@
 package ru.trylogic.spring.boot.thrift.aop;
 
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import lombok.RequiredArgsConstructor;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 
+import java.util.concurrent.TimeUnit;
+
 @RequiredArgsConstructor
 public class MetricsThriftMethodInterceptor implements MethodInterceptor {
+    private static final String THRIFT_REQUEST_DURATION_METRIC = "thrift.request.duration";
 
     private final MeterRegistry meterRegistry;
 
     @Override
     public Object invoke(MethodInvocation invocation) throws Throwable {
-        long startTime = System.currentTimeMillis();
+        final long startTime = System.nanoTime();
+
         try {
-            return invocation.proceed();
+            final Object result = invocation.proceed();
+            time(invocation, startTime);
+            return result;
+        } catch (Exception e) {
+            time(invocation, startTime, e);
+            throw e;
         } finally {
             meterRegistry.gauge("timer.thrift." + invocation.getThis().getClass().getCanonicalName() + "." + invocation.getMethod().getName(), System.currentTimeMillis() - startTime);
         }
+    }
+
+    private void time(MethodInvocation invocation, long startTime) {
+        time(invocation, startTime, null);
+    }
+
+    private void time(MethodInvocation invocation, long startTime, Exception exception) {
+        Timer
+                .builder(THRIFT_REQUEST_DURATION_METRIC)
+                .publishPercentileHistogram()
+                .description("Thrift handler request duration")
+                .tags(
+                        "handler", invocation.getThis().getClass().getCanonicalName(),
+                        "method", invocation.getMethod().getName(),
+                        "status", exception == null ? "ok" : exception.getClass().getCanonicalName()
+                )
+                .register(meterRegistry)
+                .record(System.nanoTime() - startTime, TimeUnit.NANOSECONDS);
     }
 }
