280e65d3a08b94021ec522342121b529a84ce803
Mikhail Yakushin <driver733@gmail.com>
2020-05-13 11:43:45 +0300

    feat: make thrift clients be registered in the app context as singleton beans

--- autoconfigure/src/main/java/info/developerblog/spring/thrift/client/ThriftClientBeanPostProcessor.java
+++ autoconfigure/src/main/java/info/developerblog/spring/thrift/client/ThriftClientBeanPostProcessor.java
@@ -1,27 +1,12 @@
 package info.developerblog.spring.thrift.client;
 
 import info.developerblog.spring.thrift.annotation.ThriftClient;
-import info.developerblog.spring.thrift.client.pool.ThriftClientKey;
-import java.lang.reflect.Field;
-import java.lang.reflect.UndeclaredThrowableException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import lombok.SneakyThrows;
-import org.aopalliance.intercept.MethodInterceptor;
-import org.apache.commons.pool2.KeyedObjectPool;
-import org.apache.thrift.TException;
-import org.apache.thrift.TServiceClient;
-import org.apache.thrift.protocol.TProtocol;
 import org.springframework.aop.framework.Advised;
-import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.support.AopUtils;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.InvalidPropertyException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
@@ -29,6 +14,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ReflectionUtils;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by aleksandr on 01.09.15.
@@ -38,15 +29,13 @@ import org.springframework.util.ReflectionUtils;
 @Configuration
 @ConditionalOnClass(ThriftClient.class)
 @ConditionalOnWebApplication
-@AutoConfigureAfter(PoolConfiguration.class)
-public class ThriftClientBeanPostProcessor implements org.springframework.beans.factory.config.BeanPostProcessor {
-    private Map<String, List<Class>> beansToProcess = new HashMap<>();
+@AutoConfigureAfter(ThriftClientBeanPostProcessorService.class)
+public class ThriftClientBeanPostProcessor implements BeanPostProcessor {
+
+    private final Map<String, List<Class>> beansToProcess = new HashMap<>();
 
     @Autowired
-    private DefaultListableBeanFactory beanFactory;
-
-    @Autowired
-    private KeyedObjectPool<ThriftClientKey, TServiceClient> thriftClientsPool;
+    private ThriftClientBeanPostProcessorService service;
 
     public ThriftClientBeanPostProcessor() {
     }
@@ -75,22 +64,13 @@ public class ThriftClientBeanPostProcessor implements org.springframework.beans.
             for (Class clazz : beansToProcess.get(beanName)) {
                 for (Field field : clazz.getDeclaredFields()) {
                     ThriftClient annotation = AnnotationUtils.getAnnotation(field, ThriftClient.class);
-
                     if (null != annotation) {
-                        if (beanFactory.containsBean(field.getName())) {
-                            ReflectionUtils.makeAccessible(field);
-                            ReflectionUtils.setField(field, target, beanFactory.getBean(field.getName()));
-                        } else {
-                            ProxyFactory proxyFactory = getProxyFactoryForThriftClient(target, field);
-
-                            addPoolAdvice(proxyFactory, annotation);
-
-                            proxyFactory.setFrozen(true);
-                            proxyFactory.setProxyTargetClass(true);
-
-                            ReflectionUtils.makeAccessible(field);
-                            ReflectionUtils.setField(field, target, proxyFactory.getProxy());
-                        }
+                        ReflectionUtils.makeAccessible(field);
+                        ReflectionUtils.setField(
+                                field,
+                                target,
+                                service.getThriftClientInstance(field.getType(), annotation)
+                        );
                     }
                 }
             }
@@ -103,53 +83,9 @@ public class ThriftClientBeanPostProcessor implements org.springframework.beans.
     private Object getTargetBean(Object bean) {
         Object target = bean;
         while (AopUtils.isAopProxy(target)) {
-            target = ((Advised)target).getTargetSource().getTarget();
+            target = ((Advised) target).getTargetSource().getTarget();
         }
         return target;
-    }
-
-    private ProxyFactory getProxyFactoryForThriftClient(Object bean, Field field) {
-        ProxyFactory proxyFactory = null;
-        try {
-            proxyFactory = new ProxyFactory(
-                    BeanUtils.instantiateClass(
-                            field.getType().getConstructor(TProtocol.class),
-                            (TProtocol) null
-                    )
-            );
-        } catch (NoSuchMethodException e) {
-            throw new InvalidPropertyException(bean.getClass(), field.getName(), e.getMessage());
-        }
-        return proxyFactory;
-    }
-
-    @SuppressWarnings("unchecked")
-    private void addPoolAdvice(ProxyFactory proxyFactory, ThriftClient annotation) {
-        proxyFactory.addAdvice((MethodInterceptor) methodInvocation -> {
-            Object[] args = methodInvocation.getArguments();
-
-            Class<? extends TServiceClient> declaringClass = (Class<? extends TServiceClient>) methodInvocation.getMethod().getDeclaringClass();
-
-            TServiceClient thriftClient = null;
-
-            ThriftClientKey key = ThriftClientKey.builder()
-                    .clazz(declaringClass)
-                    .serviceName(annotation.serviceId())
-                    .path(annotation.path())
-                    .build();
-
-            try {
-                thriftClient = thriftClientsPool.borrowObject(key);
-                return ReflectionUtils.invokeMethod(methodInvocation.getMethod(), thriftClient, args);
-            } catch (UndeclaredThrowableException e) {
-                if (TException.class.isAssignableFrom(e.getUndeclaredThrowable().getClass()))
-                    throw (TException)e.getUndeclaredThrowable();
-                throw e;
-            } finally {
-                if (null != thriftClient)
-                    thriftClientsPool.returnObject(key, thriftClient);
-            }
-        });
     }
 
 }
